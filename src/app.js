b4ba2d89a440c9a3dc61ca1a91917a31dfaf74f2
albertoZG <albertozg94@gmail.com>
2025-06-21 21:20:33 +0000

    big bangT game

--- src/app.js
+++ src/app.js
@@ -1,3 +1,42 @@
 //write your code here
 console.log("Hello Rigo from the console!");
 
+const manos = ["rock", "paper", "scissors", "lizard", "spock"];
+
+const reglas = {
+    rock: ["scissors", "lizard"],
+    paper: ["rock", "spock"],
+    scissors: ["paper", "lizard"],
+    lizard: ["paper", "spock"],
+    spock: ["scissors", "rock"],
+}
+
+
+const getRandomManos = () => {
+    const index = Math.floor(Math.random() * manos.length)
+    return manos[index]
+
+}
+
+const checkManosGuess = (userGuess) => {
+    const seleccionCompu = getRandomManos()
+console.log (reglas[userGuess])
+    if (seleccionCompu == userGuess) {
+        console.log("empatastes desgraciado")
+        return
+    }
+
+
+    if (reglas[userGuess].includes(seleccionCompu)) { 
+        console.log ("ganastes perro")
+
+
+    } else {
+        console.log("perdiste de nuevo")
+    }
+    
+
+
+}
+
+ checkManosGuess("paper")
